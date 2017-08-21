c6552506ea9c7ed6956c40a72ffb723a4ecd7305
Robin Deteix <rdeteix@linagora.com>
2017-08-21 17:09:16 +0200

    linagora/lgs/openpaas/Hubl.in-Scalable-VideoConference#15 & linagora/lgs/openpaas/Hubl.in-Scalable-VideoConference#3 Added connect function : Janus init and attach

--- frontend/js/services/janusRTCAdapter.js
+++ frontend/js/services/janusRTCAdapter.js
@@ -4,14 +4,41 @@ angular.module('hublin.janus.connector')
   .constant('JANUS_CONSTANTS', {
     join: 'join',
     defaultRoom: 1234,
-    publisher: 'publisher'
+    publisher: 'publisher',
+    serverAddress: 'http://localhost:8088/janus',
+    videoroom: 'janus.plugin.videoroom'
   })
 
   .factory('janusRTCAdapter', function(session, JANUS_CONSTANTS) {
 
     return {
+      connect: connect,
       handleSuccessAttach: handleSuccessAttach
     };
+
+    function connect() {
+      Janus.init({
+        debug: true,
+        callback: function() {
+          var janus = new Janus({
+            server: JANUS_CONSTANTS.serverAddress,
+            success: function() {
+              Janus.debug('Session created!');
+              janus.attach({
+                plugin: JANUS_CONSTANTS.videoroom,
+                success: handleSuccessAttach,
+                error: function(error) {
+                  Janus.debug('Error: ' + error);
+                }
+              });
+            },
+            error: function(error) {
+              Janus.debug('Error while creating the session: ' + error);
+            }
+          });
+        }
+      });
+    }
 
     function handleSuccessAttach(pluginHandle) {
       var username = session.getUsername();
@@ -21,4 +48,5 @@ angular.module('hublin.janus.connector')
 
       pluginHandle.send({ message: register });
     }
+
   });
