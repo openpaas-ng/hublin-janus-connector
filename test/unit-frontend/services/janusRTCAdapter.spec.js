2c977c93f90a498a51906fb450d7210b1764b225
Robin Deteix <rdeteix@linagora.com>
2017-08-23 18:19:47 +0200

    linagora/lgs/openpaas/Hubl.in-Scalable-VideoConference#15 Writing unit tests for connect()

--- test/unit-frontend/services/janusRTCAdapter.spec.js
+++ test/unit-frontend/services/janusRTCAdapter.spec.js
@@ -4,18 +4,83 @@ var expect = chai.expect;
 var sinon = sinon;
 
 describe('janusAdapter service', function() {
-  var session, janusRTCAdapter;
+  var session, janusRTCAdapter, janusFactory;
 
   beforeEach(function() {
     angular.mock.module('hublin.janus.connector', function($provide) {
       $provide.value('session', session);
+      $provide.value('janusFactory', janusFactory);
     });
     session = {
       getUsername: function() { return 'Woot';}
     };
+    janusFactory = {};
     angular.mock.inject(function(_janusRTCAdapter_) {
       janusRTCAdapter = _janusRTCAdapter_;
     });
+
+  });
+
+  describe('The connect method', function() {
+    it('should call the init method of Janus', function() {
+      janusFactory.get = function() {
+        var Janus = {};
+
+        Janus.init = function(object) {
+          expect(object.debug).to.be.true;
+          expect(object.callback).to.be.a('function');
+        };
+
+        return Janus;
+      };
+
+      janusRTCAdapter.connect();
+    });
+
+    it('should create a new Janus object', function() {
+      janusFactory.get = function() {
+        var Janus = function(object) {
+          expect(object.server).to.equal('http://localhost:8088/janus');
+          expect(object.success).to.be.a('function');
+          expect(object.error).to.be.a('function');
+        };
+
+        Janus.init = function(object) {
+          object.callback();
+        };
+
+        return Janus;
+      };
+
+      janusRTCAdapter.connect();
+    });
+
+    it('should call janus.attach if Janus session has been successfully created', function(done) {
+      janusFactory.get = function() {
+        var Janus = function(object) {
+          this.attach = function(object) {
+            expect(object.plugin).to.equal('janus.plugin.videoroom');
+            expect(object.success).to.be.a('function');
+            expect(object.error).to.be.a('function');
+            done();
+          };
+          //Timeout is necessary because the janus.js API is built that way :
+          //the Janus function builds the object, then it calls the success callback which in turn calls the object that was just built.
+          //if I do not add the timeout the success callback is executed before the object has been created
+          setTimeout(object.success, 0);
+        };
+
+        Janus.init = function(object) {
+          object.callback();
+        };
+        Janus.debug = function() {};
+
+        return Janus;
+      };
+
+      janusRTCAdapter.connect();
+    });
+
   });
 
   describe('handleSuccessAttach method', function() {
