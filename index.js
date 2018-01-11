87fe45ee6b6c8d9c586661729eb4fe233cd6cfba
Christophe Hamerling <chamerling@linagora.com>
2018-01-11 12:11:06 +0100

    linagora/lgs/openpaas/hublin#526 Clean module index

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const AwesomeModule = require('awesome-module');
 const Dependency = AwesomeModule.AwesomeModuleDependency;
+const MODULE_NAME = 'hublin.janus.connector';
+const APP_NAME = 'connectorjanus';
 
-const janusConnector = new AwesomeModule('hublin.janus.connector', {
+const janusConnector = new AwesomeModule(MODULE_NAME, {
   dependencies: [
     new Dependency(Dependency.TYPE_ABILITY, 'wsserver', 'wsserver'),
     new Dependency(Dependency.TYPE_NAME, 'webserver.wrapper', 'webserver-wrapper'),
     new Dependency(Dependency.TYPE_NAME, 'linagora.io.meetings.core.logger', 'logger')
   ],
-  //abilities: ['hublin.webrtc.connector'],
+  abilities: ['webrtc-adapter'],
   states: {
     lib: (dependencies, callback) => {
       const lib = {
@@ -21,9 +23,9 @@ const janusConnector = new AwesomeModule('hublin.janus.connector', {
       const app = require('./backend/webserver/application')();
       const webserverWrapper = dependencies('webserver-wrapper');
 
-      webserverWrapper.injectAngularModules('connectorjanus', ['app.js', 'services/janusRTCAdapter.js'], 'hublin.janus.connector', ['live-conference']);
+      webserverWrapper.injectAngularModules(APP_NAME, ['app.js', 'services/janusRTCAdapter.js'], MODULE_NAME, ['live-conference']);
       webserverWrapper.injectJSAsset('connector', ['connectorjanus/js/janus.js'], ['connector']);
-      webserverWrapper.addApp('connectorjanus', app);
+      webserverWrapper.addApp(APP_NAME, app);
 
       callback();
     }
